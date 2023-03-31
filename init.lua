11a6451e5e2d659f567c36c2d2ba70a9c12b9d69
jacobmacwilliams <jacobmacwilliams@gmail.com>
2023-03-31 12:55:48 +0200

    Add keybindings file and require it in init.lua.
    The leader key and simple mappings with the leader key for writing and
    quitting nvim were added to the bindings.lua file.

--- init.lua
+++ init.lua
@@ -3,3 +3,5 @@ vim.opt.relativenumber = true
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 0
 vim.opt.expandtab = true
+
+require('keybindings.bindings')
---
+++ lua/keybindings/bindings.lua
@@ -0,0 +1,4 @@
+vim.g.mapleader = " "
+
+vim.keymap.set('n', '<leader>w', ':w<CR>',{noremap=true})
+vim.keymap.set('n', '<leader>q', ':q<CR>',{noremap=true}) 
