a4edb10a031f9fd4ab254d973679b44875d22a95
jacobmacwilliams <jacobmacwilliams@gmail.com>
2023-03-31 16:31:55 +0200

    Add treesitter to plugins.

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,9 +1,18 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
+
         -- Packer can manage itself
-        use 'wbthomson/packer.nvim'
+        use 'wbthomason/packer.nvim'
 
-        -- Add plugins here
+        -- Semantic tree creation of buffer contents and syntax
+        -- highlightin   
+        use {
+          'nvim-treesitter/nvim-treesitter',
+          run = function()
+               local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
+               ts_update()
+             end,
+        }
+        
 end)
-
