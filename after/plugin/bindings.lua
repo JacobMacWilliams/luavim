a476482a25b7d6b3465317c8c7dc31b15885c1b1
jacobmacwilliams <jacobmacwilliams@gmail.com>
2023-04-01 17:44:39 +0200

    Add some basic mappings.

--- after/plugin/bindings.lua
+++ after/plugin/bindings.lua
@@ -1,4 +1,7 @@
 vim.g.mapleader = " "
 
-vim.keymap.set('n', '<leader>w', ':w<CR>',{noremap=true})
-vim.keymap.set('n', '<leader>q', ':q<CR>',{noremap=true}) 
+vim.keymap.set('n', '<leader>w', ':w<CR>', {noremap=true})
+vim.keymap.set('n', '<leader>q', ':q<CR>', {noremap=true})
+vim.keymap.set('n', '<leader>b', ':bn<CR>', {noremap=true})
+vim.keymap.set('i', '<leader>i','<Esc>', {noremap=true})
+vim.keymap.set('n', '<Esc>', ':noh<CR>', {noremap=true})
