fb095476531ac1755422860e3192b4abc9c68996
jacobmacwilliams <jacobmacwilliams@gmail.com>
2023-04-01 13:48:20 +0200

    Add tokyonight colortheme.

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -27,9 +27,12 @@ return require('packer').startup(function(use)
           'nvim-lualine/lualine.nvim',
            requires = { 'nvim-tree/nvim-web-devicons', opt = true }
        }
-        
+       
+       -- Hop: Intra-File navigation tool.
        use {
           'phaazon/hop.nvim',
           branch = 'v2', -- optional but strongly recommended
         }
+
+       use 'folke/tokyonight.nvim'
 end)
