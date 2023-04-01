9407050b60eec23e7974656bb36ccfdb072fe3b0
jacobmacwilliams <jacobmacwilliams@gmail.com>
2023-04-01 19:34:52 +0200

    Don't reference <leader> in insert.

--- after/plugin/bindings.lua
+++ after/plugin/bindings.lua
@@ -3,5 +3,5 @@ vim.g.mapleader = " "
 vim.keymap.set('n', '<leader>w', ':w<CR>', {noremap=true})
 vim.keymap.set('n', '<leader>q', ':q<CR>', {noremap=true})
 vim.keymap.set('n', '<leader>b', ':bn<CR>', {noremap=true})
-vim.keymap.set('i', '<leader>i','<Esc>', {noremap=true})
+vim.keymap.set('i', 'uu','<Esc>', {noremap=true})
 vim.keymap.set('n', '<Esc>', ':noh<CR>', {noremap=true})
