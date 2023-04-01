09a43d28a4f4150ac5d3b7e4f904380490a481c2
jacobmacwilliams <jacobmacwilliams@gmail.com>
2023-04-01 19:02:12 +0200

    Fix jdtls reference.

--- after/plugin/lsp_zero.lua
+++ after/plugin/lsp_zero.lua
@@ -6,9 +6,8 @@ end)
 
 -- When you don't have mason.nvim installed
 -- You'll need to list the servers installed in your system
-lsp.setup_servers({'tsserver', 'eslint'})
+lsp.setup_servers({'jdtls', 'julia-lsp'})
 
 -- (Optional) Configure lua language server for neovim
 require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
-
 lsp.setup()
