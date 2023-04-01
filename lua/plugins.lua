2e765cc17241f1e1acd42ae6fe347492a812a6af
jacobmacwilliams <jacobmacwilliams@gmail.com>
2023-04-01 14:26:33 +0200

    Add basic lsp functionality with lsp-zero.

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -14,6 +14,26 @@ return require('packer').startup(function(use)
                ts_update()
              end,
         }
+        use {
+          'VonHeikemen/lsp-zero.nvim',
+          branch = 'v2.x',
+          requires = {
+            -- LSP Support
+            {'neovim/nvim-lspconfig'},             -- Required
+            {                                      -- Optional
+              'williamboman/mason.nvim',
+              run = function()
+                pcall(vim.cmd, 'MasonUpdate')
+              end,
+            },
+            {'williamboman/mason-lspconfig.nvim'}, -- Optional
+
+            -- Autocompletion
+            {'hrsh7th/nvim-cmp'},     -- Required
+            {'hrsh7th/cmp-nvim-lsp'}, -- Required
+            {'L3MON4D3/LuaSnip'},     -- Required
+        }
+}
 
         -- Telescope: Fuzzy finder for various lists 
        use {
