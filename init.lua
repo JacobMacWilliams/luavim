a19847eb8f42e89450da990ede4f9c74a5e2d78a
jacobmacwilliams <jacobmacwilliams@gmail.com>
2023-04-01 12:42:06 +0200

    Add after directory and move keybindings.

--- init.lua
+++ init.lua
@@ -4,15 +4,9 @@ vim.opt.tabstop = 4
 vim.opt.softtabstop = 0
 vim.opt.expandtab = true
 
-require('keybindings.bindings')
 require('plugins')
 
 require('configs.treesitter')
-
 require('configs.telescope')
-require('keybindings.telescope')
-
 require('configs.lualine')
-
 require('configs.hop')
-require('keybindings.hop')
