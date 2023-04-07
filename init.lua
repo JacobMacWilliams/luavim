7e2e29db836182e610e31bd94c026d1c573bcf7c
jacobmacwilliams <jacobmacwilliams@gmail.com>
2023-04-07 10:59:33 +0200

    Configure tab settings.

--- init.lua
+++ init.lua
@@ -1,8 +1,13 @@
+-- Line number settings
 vim.opt.number = true
 vim.opt.relativenumber = true
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 0
+
+-- Tab settings
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
+vim.opt.softtabstop = 0
+
 vim.opt.timeoutlen = 200
 
 require('plugins')
