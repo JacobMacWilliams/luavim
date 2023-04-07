26d2ec75d968105a020b671f2b6ffa4d9b4f2cd8
jacobmacwilliams <jacobmacwilliams@gmail.com>
2023-04-07 11:09:42 +0200

    Configure line wrapping behavior.

--- init.lua
+++ init.lua
@@ -8,6 +8,11 @@ vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 vim.opt.softtabstop = 0
 
+-- Line wrapping
+vim.opt.textwidth = 80
+vim.opt.wrap = true
+vim.opt.linebreak = true
+
 vim.opt.timeoutlen = 200
 
 require('plugins')
