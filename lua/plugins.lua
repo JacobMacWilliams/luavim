b3039b4c822ee64f4301e7bb324bc06eed49e1dd
jacobmacwilliams <jacobmacwilliams@gmail.com>
2023-09-25 22:16:27 +0200

    Update lspzero version to get completions working again.

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -17,7 +17,7 @@ return require('packer').startup(function(use)
         -- components.
         use {
           'VonHeikemen/lsp-zero.nvim',
-          branch = 'v2.x',
+          branch = 'v3.x',
           requires = {
             -- LSP Support
             {'neovim/nvim-lspconfig'},             -- Required
