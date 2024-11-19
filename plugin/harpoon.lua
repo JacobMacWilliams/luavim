873a18beca3fbb72a537cd49bfa70339389a3a06
JacobMacWilliams <jacobmacwilliams@gmail.com>
2024-11-19 18:26:53 +0100

    Update harpoon keybindings.

--- plugin/harpoon.lua
+++ plugin/harpoon.lua
@@ -9,7 +9,7 @@ harpoon:setup({
 vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
 vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
-vim.keymap.set("n", "<C-u>", function() harpoon:list():select(1) end)
-vim.keymap.set("n", "<C-i>", function() harpoon:list():select(2) end)
-vim.keymap.set("n", "<C-o>", function() harpoon:list():select(3) end)
-vim.keymap.set("n", "<C-p>", function() harpoon:list():select(4) end)
+vim.keymap.set("n", "1", function() harpoon:list():select(1) end)
+vim.keymap.set("n", "2", function() harpoon:list():select(2) end)
+vim.keymap.set("n", "3", function() harpoon:list():select(3) end)
+vim.keymap.set("n", "4", function() harpoon:list():select(4) end)
