6c0d8f48dc45a2ae29a6bb37b4c4f59b53d18222
JacobMacWilliams <jacobmacwilliams@gmail.com>
2024-12-07 11:52:53 +0100

    Remove auto line formatting.

--- init.lua
+++ init.lua
@@ -9,9 +9,9 @@ vim.opt.expandtab = false
 vim.opt.softtabstop = 0
 
 -- Line wrapping
-vim.opt.textwidth = 92
-vim.opt.wrap = true
-vim.opt.linebreak = true
+--vim.opt.textwidth = 92
+--vim.opt.wrap = true
+--vim.opt.linebreak = true
 vim.opt.breakat = " "
 
 vim.opt.timeoutlen = 200
