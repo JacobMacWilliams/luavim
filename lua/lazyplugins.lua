f1fd8e00b579d8e60f2e3b075f5645b474ac8315
JacobMacWilliams <jacobmacwilliams@gmail.com>
2025-07-20 10:34:08 +0200

    Add lazydev for lua lsp configuration.

--- lua/lazyplugins.lua
+++ lua/lazyplugins.lua
@@ -69,7 +69,27 @@ return {
 
 	--LSP-Config
 	{'neovim/nvim-lspconfig'},
-    {'hrsh7th/nvim-cmp'},     -- Required
+	{ --required for lua lsp configuration
+    	"folke/lazydev.nvim",
+    	ft = "lua", -- only load on lua files
+    	opts = {
+    	  library = {
+    	    -- See the configuration section for more details
+    	    -- Load luvit types when the `vim.uv` word is found
+    	    { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+    	  },
+    	},
+  	},
+    {
+		'hrsh7th/nvim-cmp',
+    	opts = function(_, opts) -- required for lazydev
+    	  opts.sources = opts.sources or {}
+    	  table.insert(opts.sources, {
+    	    name = "lazydev",
+    	    group_index = 0, -- set group index to 0 to skip loading LuaLS completions
+    	  })
+    	end,
+	},     -- Required
     {'hrsh7th/cmp-nvim-lsp'}, -- Required
     {'L3MON4D3/LuaSnip'},     -- Required
 	{
